a5db36b09e7324fc63b1811c0496e100c97bbf33
LG West <larsgwest@gmail.com>
2024-08-03 07:15:33 +0200

    current pico code

---
+++ src/main.py
@@ -0,0 +1,43 @@
+# Rui Santos & Sara Santos - Random Nerd Tutorials
+# Complete project details at https://RandomNerdTutorials.com/raspberry-pi-pico-w-micropython-ebook/
+
+import machine, onewire, ds18x20, time
+
+# Pin configuration for DS18B20 temperature sensor
+ds_pin = machine.Pin(22)
+led = machine.Pin(0, machine.Pin.OUT)
+led.value(1)
+
+# Create DS18X20 object using OneWire protocol with specified pin
+ds_sensor = ds18x20.DS18X20(onewire.OneWire(ds_pin))
+
+def celsius_to_fahrenheit(temp_celsius):
+    # Convert temperature from Celsius to Fahrenheit
+    temp_fahrenheit = temp_celsius * (9/5) + 32 
+    return temp_fahrenheit
+
+# Scan for DS18B20 sensors and print their ROM addresses
+roms = ds_sensor.scan()
+print('Found DS devices: ', roms)
+led.value(0)
+while True:
+    # Initiate temperature conversion for all sensors
+    ds_sensor.convert_temp()
+    time.sleep_ms(750)  # Wait for the conversion to complete (750 ms is recommended)
+    
+    for rom in roms:
+        print(rom)
+            
+        # Read temperature in Celsius from the sensor
+        temp_c = ds_sensor.read_temp(rom)
+        if rom == roms[0] and temp_c > 25:
+            led.value(0)
+        elif rom == roms[0] and temp_c < 24:
+            led.value(1)
+        
+
+        # Print the temperature readings
+        print('temperature (ºC):', "{:.2f}".format(temp_c))
+
+    time.sleep(1)  # Wait for 5 seconds before taking readings again
+
